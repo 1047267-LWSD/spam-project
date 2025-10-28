802fc34c303159659d4bd8104fa6ccbbea56de95
Riya <155154244+r-i-y-aa@users.noreply.github.com>
2025-10-28 01:07:46 -0700

    Initial commit

--- backend/server.py
+++ backend/server.py
@@ -44,6 +44,6 @@ def get_predict():
 def forum():
     return render_template('forum.html')
 
-if __name__ == '__main__':
-    serve(app, host = "127.0.0.1", port = 8000)
+if __name__ == "__main__":
+    app.run()
 
