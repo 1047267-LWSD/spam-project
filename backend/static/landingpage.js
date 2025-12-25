62d78bc71d17c9598c30cc0691ddd443162174a3
r-i-y-aa <ri.kulkarni02@gmail.com>
2025-12-25 15:07:57 -0800

    fixed landing page logic

--- backend/static/landingpage.js
+++ backend/static/landingpage.js
@@ -1,3 +1,5 @@
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { db } from './firebase-config.js';
  async function loadDocs() {
         const collectionRef = collection(db, 'reports');
         const docs = await getDocs(collectionRef);
@@ -8,4 +10,4 @@
         return numTimes;
         
     }
-document.querySelector('.stats-number').textContent = `${await loadDocs()}`;
+document.querySelector('#numTimes').textContent = `${await loadDocs()}`;
